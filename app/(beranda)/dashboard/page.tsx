f00177648443653b0ad4896d19b71064b492d76f
NamkuGavin <m.gavin.aga10@gmail.com>
2026-02-16 17:41:29 +0700

    update halaman dashboard

--- app/(beranda)/dashboard/page.tsx
+++ app/(beranda)/dashboard/page.tsx
@@ -1,3 +1,31 @@
+"use client";
+import Image from "next/image";
+import HeaderDashboard from "./components/header_dashboard";
+import FooterDashboard from "./components/footer_dahsboard";
+import ChartStatisLoginUser from "./components/chart_statis_login_user";
+import StatTelkom from "./components/stat_telkom";
+import Leaderboard from "./components/leaderboard";
+
 export default function Dashboard() {
-  return <div className="text-2xl text-gray-600">Dashboard Content</div>;
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-50 text-gray-800">
+      <HeaderDashboard />
+      <div className="flex justify-between items-center my-2">
+        <h1 className="text-lg">Aktivitas Anda</h1>
+        <Image
+          src="/Assets/helphin_1.png"
+          alt="helphin_1"
+          width={75}
+          height={75}
+          priority
+        />
+      </div>
+      <div className="flex">
+        <ChartStatisLoginUser />
+        <StatTelkom />
+      </div>
+      <Leaderboard />
+      <FooterDashboard />
+    </div>
+  );
 }
